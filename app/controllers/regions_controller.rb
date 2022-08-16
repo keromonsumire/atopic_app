1a1affab0c881bbd416eae7b97c2a62c64f979d6
田島和樹 <kazukitashima@tajimakazukinoMacBook-Air.local>
2022-08-16 11:28:44 +0900

    regionの編集機能と削除機能の実装

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -12,12 +12,36 @@ class RegionsController < ApplicationController
     def create
         @user = current_user
         @region = @user.regions.create(params.require(:region).permit(:name, :interval, :time))
-        if @region
+        @region.start = Time.current
+        if @region.save
             redirect_to "/regions/show"
             flash[:success] = "新しい部位を登録しました！"
         else
             flash[:notice] = "部位登録に失敗しました"
             render 'new'
         end
+    end
+
+    def destroy
+        if Region.find(params[:id]).destroy
+            flash[:success] = "部位を削除しました"
+        else
+            flash[:danger] = "部位削除に失敗しました"
+        end
+        redirect_to "/regions/show"
+    end
+
+    def edit
+        @region = Region.find(params[:id])
+    end
+
+    def update
+        @region = Region.find(params[:id])
+        if @region.update(params.require(:region).permit(:name, :interval, :time))
+            flash[:success] = "部位情報をアップデートしました"
+        else
+            flash[:danger] = "部位情報のアップデートに失敗しました"
+        end
+        redirect_to "/regions/show"
     end
 end
