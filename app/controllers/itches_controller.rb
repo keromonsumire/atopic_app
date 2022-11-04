4a2e6a1720359b365d07f160544fea9f79c7ba17
田島和樹 <kazuki01160938@yahoo.co.jp>
2022-11-04 21:02:42 +0900

    かゆみを記録する際にプロアクティブスタートを現在の日付に

--- app/controllers/itches_controller.rb
+++ app/controllers/itches_controller.rb
@@ -2,12 +2,12 @@ class ItchesController < ApplicationController
   require 'date'
   def create
       @region = Region.find(params[:id])
-      if Time.current.strftime('%H').to_i > 3 #深夜4時以降
+      if Time.current.strftime('%H').to_i > 3 
         @region.itches.create({ date: Date.current }) if !Itch.exists?(region_id: @region.id, date: Date.current)
-      else #深夜3時59分まで
+      else #深夜例外
         @region.itches.create({ date: Date.current.yesterday }) if !Itch.exists?(region_id: @region.id, date: Date.current.yesterday)
       end
-      @region.update(interval:1)
+      @region.update(interval:1, proactive_start:Date.current)
       redirect_to regions_path
       flash[:success] = "かゆみが記録されました!"
   end
